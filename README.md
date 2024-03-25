e3e431b0e43f4d6b4d371de307bab31e7c85811a
Sidd-77 <siddharthsalunkhe2003@gmail.com>
2024-03-25 12:18:44 +0530

    minor changes

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# style-transfer
+# Neural Style Transfer
+
+This project is a Neural Style Transfer application that allows users to apply the style of one image to another. It uses a pre-trained model from TensorFlow Hub to perform the style transfer.
+
+## Tech Stack
+
+- **Streamlit**: A fast, efficient, and easy-to-use framework for building web applications.
+- **TensorFlow**: An end-to-end open source platform for machine learning.
+- **TensorFlow Hub**: A repository of pre-trained TensorFlow models.
+
+## Running the Project Locally
+
+1. Clone the repository to your local machine:
+
+```bash
+git clone https://github.com/Sidd-77/style-transfer.git
+```
+
+2. Navigate to the project directory:
+
+```bash
+cd style-transfer
+```
+
+3. Install required dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+4. Run the Streamlit application:
+
+```bash
+streamlit run app.py
+```
+
+The application should be now running at ```http://localhost:8501```
--- app.py
+++ app.py
@@ -2,25 +2,23 @@ import streamlit as st
 import os
 import sys
 sys.path.insert(0, './')
+
 from src.components.data_ingestion import DataIngestion, DataIngestionConfig
 from src.pipeline.train_pipeline import TrainPipeline, TrainConfig
 st.title(':blue[Neural :green[Style] Transfer]')
+st.markdown('This project is a Neural Style Transfer application that allows users to apply the style of one image to another. ')
+st.divider()
 
-st.slider("How much proportion to apply style : ", min_value=1, max_value=5, step=1)
 
 content_file = st.file_uploader("Choose content image", type=['jpg', 'png', 'jpeg'])
 if content_file is not None:
-    st.image(content_file)
-    # print(type(content_file))
-    # with open(os.path.join("tempDir",content_file.name),"wb+") as f:
-    #      f.write(content_file.getbuffer())
+    st.image(content_file, width=512)
 
 
 style_file = st.file_uploader("Choose style image", type=['jpg', 'png', 'jpeg'])
 if style_file is not None:
-    st.image(style_file)
-    # with open(os.path.join("tempDir",style_file.name),"wb+") as f:
-    #      f.write(style_file.getbuffer())
+    st.image(style_file, width=512)
+
 
 if st.button("Start it..."):
      obj = TrainPipeline()
